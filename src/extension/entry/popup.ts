7e37ac1500478f877b89a0817af1bbe4bf81cb3e
Евгений Галимов <egalimov95@gmail.com>
2022-08-19 11:28:00 +0300

    fix serve script add missing cryptowaitready

--- src/extension/entry/popup.ts
+++ src/extension/entry/popup.ts
@@ -1,15 +1,31 @@
-import Vue from 'vue';
 import App from '@/App.vue';
+import keyring from '@polkadot/ui-keyring';
 import router from '@/router';
 import store from '@/store';
+import Vue from 'vue';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
 import '@/styles';
 import '@/plugins';
+
+// import AccountsStore from './storeChrome/Accounts';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = process.env.NODE_ENV === 'development';
 
-new Vue({
-  store,
-  router,
-  render: (h) => h(App),
-}).$mount('#app');
+cryptoWaitReady().then((): void => {
+  console.info('crypto initialized');
+
+  // load all the keyring data
+  keyring.loadAll({
+    // store: new AccountsStore(),
+    type: 'sr25519',
+  });
+
+  console.info('initialization completed');
+
+  new Vue({
+    store,
+    router,
+    render: (h) => h(App),
+  }).$mount('#app');
+});

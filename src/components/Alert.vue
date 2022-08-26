3711595365ab76ed481a21b3b96c3f07f816d1bd
Евгений Галимов <egalimov95@gmail.com>
2022-08-26 15:05:45 +0600

    fix alert borders

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,10 +1,12 @@
 <template>
-  <Corners class="alert-container" isWarning size="big">
-    <div class="alert__content">
-      <Hint class="alert__header" size="big" iconName="warning-orange" text="Attention" />
-      <p class="alert__message">
-        {{ message }}
-      </p>
+  <Corners isWarning size="big">
+    <div class="alert-container">
+      <div class="alert__content">
+        <Hint class="alert__header" size="big" iconName="warning-orange" text="Attention" />
+        <p class="alert__message">
+          {{ message }}
+        </p>
+      </div>
     </div>
   </Corners>
 </template>

9377e4284d6406feda3dfa47411bd1e0cc5f9360
Евгений Галимов <egalimov95@gmail.com>
2022-09-05 16:12:06 +0300

    layout fix for athorize screen

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
 <script lang="ts">
 import { keyring } from '@polkadot/ui-keyring';
 import { Component, Vue } from 'vue-property-decorator';
-import { Mutation } from 'vuex-class';
-import store from './store';
+import { Action, Mutation } from 'vuex-class';
+
 import BaseApi from './util/BaseApi';
 import type { SetSelectedWalletProps, setAccountsProps } from '@/store/accounts/types';
 import type { TMutation } from '@/interfaces/common';
@@ -26,7 +26,9 @@ import { accountController } from '@/controllers/accountController';
 @Component
 export default class App extends Vue {
   subscribeAccounts!: BehaviorSubject<SubjectInfo>;
-
+  @Action(MetaActionTypes.SUBSCRIBE_TO_METADATA_REQUESTS) metaSubscribe!: () => Promise<void>;
+  @Action(AuthActionTypes.SUBSCRIBE_TO_DAPP_EVENTS) authSubscribe!: () => Promise<void>;
+  @Action(SignActionTypes.SUBSCRIBE_SIGN_EVENTS) signSubscribe!: () => Promise<void>;
   @Mutation(AccountsMutationTypes.SET_SELECTED_WALLET) setSelectedWallet!: TMutation<SetSelectedWalletProps>;
   @Mutation(AccountsMutationTypes.SET_ACCOUNTS) setAccounts!: TMutation<setAccountsProps>;
 
@@ -38,9 +40,9 @@ export default class App extends Vue {
     let loadHistory = true;
     const { loadNetworks, loadAssets, loadFiats, loadTokensPrice, subscribeToBalancesOfNetworks } = NetworksController;
 
-    await store.dispatch(AuthActionTypes.SUBSCRIBE_TO_DAPP_EVENTS); //TODO refactor to @Action
-    await store.dispatch(MetaActionTypes.SUBSCRIBE_TO_METADATA_REQUESTS); //TODO refactor to @Action
-    await store.dispatch(SignActionTypes.SUBSCRIBE_SIGN_EVENTS); //TODO refactor to @Action
+    await this.authSubscribe();
+    await this.metaSubscribe();
+    await this.signSubscribe();
 
     await Promise.all([loadNetworks(), loadAssets(), loadFiats()]);
     await loadTokensPrice();

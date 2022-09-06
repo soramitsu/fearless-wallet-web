5f4627b3e4c04a10d446b4ec65400654ba3a39a0
Евгений Галимов <egalimov95@gmail.com>
2022-09-06 16:25:58 +0300

    fix alert borders, add no account message

--- src/screens/authorize/Authorize.vue
+++ src/screens/authorize/Authorize.vue
@@ -1,6 +1,6 @@
 <template>
   <AboveForm :blur="true" header="Authorize" :showCloseIcon="false">
-    <div class="authorize">
+    <div v-if="isAccountsExists" class="authorize">
       <div>
         <p class="authorize__content">
           An application, self-identifying as
@@ -15,6 +15,10 @@
         <Button width="100%" type="link" text="Reject" size="big" fontSize="medium" @click="onReject" />
       </div>
     </div>
+    <div v-else class="authorize">
+      <Alert :message="noAccountsMessage" />
+      <Button width="100%" text="Understood" size="big" fontSize="big" @click="onReject" />
+    </div>
   </AboveForm>
 </template>
 
@@ -22,6 +26,7 @@
 import { Vue, Component } from 'vue-property-decorator';
 import { Getter } from 'vuex-class';
 import { AuthorizeRequest } from '@polkadot/extension-base/background/types';
+import { keyring } from '@polkadot/ui-keyring';
 import Button from '@/components/Button.vue';
 import Hint from '@/components/Hint.vue';
 import Alert from '@/components/Alert.vue';
@@ -40,8 +45,13 @@ import { ActionTypes } from '@/store/auth/actions';
 export default class Authorize extends Vue {
   alertMessage =
     'Only approve this request if you trust the application. Approving gives the application access to the addresses of you accounts';
+  noAccountsMessage = "You do not have any account. Please create an account and refresh the application's page.";
 
   @Getter('getRequest') requests!: AuthorizeRequest[];
+
+  get isAccountsExists() {
+    return keyring.getAccounts().length > 0;
+  }
 
   get request() {
     const [request] = this.requests;

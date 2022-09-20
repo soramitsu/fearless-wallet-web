4e195ae6fbccbf27129a68e17045e813204540b8
Maksim Baranov <maks.baranow2011@yandex.ru>
2022-09-20 12:19:10 +0300

    new icons

--- src/screens/wallet&token/SelectNetworkPopup.vue
+++ src/screens/wallet&token/SelectNetworkPopup.vue
@@ -20,7 +20,7 @@ import type { Networks as NetworksType } from '@/interfaces/networks';
 import { firstCharToUp } from '@/util/helpers';
 import SelectPopup from '@/components/SelectPopup.vue';
 import { GettersTypes as NetworksGettersTypes } from '@/store/networks/getters';
-import { getImgPathByNetworkOrTokenName } from '@/util/imgPath';
+import { getImgPath } from '@/util/imgPath';
 
 interface Options {
   label: string;
@@ -47,7 +47,7 @@ export default class SelectNetworkButton extends Vue {
         return {
           label: firstCharToUp(name),
           value: name,
-          path: `networks/${getImgPathByNetworkOrTokenName(name)}`,
+          path: getImgPath(name),
         };
       }),
     ];

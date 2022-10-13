6a418b0c372face97ffe9dd3937beb1f35945680
Евгений Галимов <egalimov95@gmail.com>
2022-10-13 12:08:34 +0600

    revive desktop build

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,15 +9,21 @@ import Asset from '@/screens/wallet&asset/asset/Asset.vue';
 import Wallet from '@/screens/wallet&asset/wallet/Wallet.vue';
 import Welcome from '@/screens/welcome/Welcome.vue';
 import AddWallet from '@/screens/addWallet/AddWallet.vue';
-import ManageAuths from '@/screens/authorize/ManageAuths.vue';
+// import ManageAuths from '@/screens/authorize/ManageAuths.vue';
 import Accounts from '@/screens/accounts/Accounts.vue';
 import AccountsLayout from '@/screens/accounts/AccountsLayout.vue';
 import Main from '@/screens/main/Main.vue';
 import Crowdloans from '@/screens/crowdloans/Crowdloans.vue';
-import Authorize from '@/screens/authorize/Authorize.vue';
-import MetaRequest from '@/screens/metadata/Metadata.vue';
-import Transaction from '@/screens/signing/Transaction.vue';
-import UpdateAuths from '@/screens/authorize/UpdateAuths.vue';
+// import Authorize from '@/screens/authorize/Authorize.vue';
+// import MetaRequest from '@/screens/metadata/Metadata.vue';
+// import Transaction from '@/screens/signing/Transaction.vue';
+// import UpdateAuths from '@/screens/authorize/UpdateAuths.vue';
+
+const ManageAuths = () => import('@/screens/authorize/ManageAuths.vue');
+const Authorize = () => import('@/screens/authorize/Authorize.vue');
+const Transaction = () => import('@/screens/signing/Transaction.vue');
+const MetaRequest = () => import('@/screens/metadata/Metadata.vue');
+const UpdateAuths = () => import('@/screens/authorize/UpdateAuths.vue');
 
 import store from '@/store';
 

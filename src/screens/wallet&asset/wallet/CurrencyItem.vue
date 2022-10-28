35c74a5279f1a5aee6843451f3535397a91d9d42
Maksim Baranov <maks.baranow2011@yandex.ru>
2022-10-28 14:32:25 +0300

    FWW-191

--- src/screens/wallet&asset/wallet/CurrencyItem.vue
+++ src/screens/wallet&asset/wallet/CurrencyItem.vue
@@ -95,6 +95,8 @@ import { GettersTypes as AccountsGettersTypes } from '@/store/accounts/getters';
   },
 })
 export default class CurrencyItem extends Vue {
+  readonly countDisplayedNetworks = 5;
+
   @Prop(Object) currency!: Currency;
   @Prop(String) selectedNetwork!: string;
   @Prop(Boolean) showAssetsManagementForm!: boolean;
@@ -162,21 +164,25 @@ export default class CurrencyItem extends Vue {
   }
 
   get availableInNetworks() {
-    return this.currency.getAvailableInNetworks(this.selectedWallet);
+    // return this.currency.getAvailableInNetworks(this.selectedWallet); // Please don't delete. Needed for development.
+
+    return this.currency.getAvailableInNetworks(this.selectedWallet).filter(({ balance: { total } }) => total !== '0');
   }
 
   get isAdditional() {
-    return this.availableInNetworks.length > 5;
+    return this.availableInNetworks.length > this.countDisplayedNetworks;
   }
 
   get additionalCount() {
-    return this.availableInNetworks.length - 4;
+    return this.availableInNetworks.length - (this.countDisplayedNetworks - 1);
   }
 
   get availableInNetworksPart() {
-    return this.isCurrentNetwork
-      ? [{ network: this.selectedNetwork }]
-      : [...this.availableInNetworks].splice(0, this.isAdditional ? 4 : 5);
+    if (this.isCurrentNetwork) return [{ network: this.selectedNetwork }];
+
+    if (this.isAdditional) return [...this.availableInNetworks].splice(0, this.countDisplayedNetworks - 1);
+
+    return this.availableInNetworks;
   }
 
   openAssetPage(event: Event) {

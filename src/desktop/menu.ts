463c5ca8ad2ab0e5fc0696023bf0f97b3161db79
Stefan Popov <stefanpopov@list.ru>
2022-11-19 03:23:29 +0100

    Fix copy paste for mac

--- src/desktop/menu.ts
+++ src/desktop/menu.ts
@@ -44,6 +44,44 @@ const defaultTemplate = (appName: string) =>
       ],
     },
     {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          selector: 'undo:',
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          selector: 'redo:',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          selector: 'cut:',
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          selector: 'copy:',
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          selector: 'paste:',
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          selector: 'selectAll:',
+        },
+      ],
+    },
+    {
       role: 'help',
       label: 'Help',
       submenu: [

dcecf55c65d020daea41e5ebc9128c5a8d1f8c6c
Maksim Baranov <maks.baranow2011@yandex.ru>
2022-11-24 11:23:33 +0300

    TypeRegistry

--- src/helpers/networksConnection.ts
+++ src/helpers/networksConnection.ts
@@ -1,4 +1,5 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
+import { TypeRegistry } from '@polkadot/types/create';
 import type { AccountData } from '@polkadot/types/interfaces/balances';
 import type { Network, ApiOptions, AssetJson } from '@/interfaces';
 import type { OrmlAccountData } from '@open-web3/orml-types/interfaces/tokens';
@@ -80,6 +81,7 @@ function connectToApi(network: Network, apiOptions: ApiOptions, _node?: Node): v
   const autoSelectNode = store.getters.getAutoSelectNodesValueByNetwork(networkName);
   const nodesList = autoSelectNode ? nodes : [activeNodes[networkName]];
   const node = _node ?? nodesList[apiOptions.nodeIndex];
+  const registry = new TypeRegistry();
 
   store.commit(MutationTypes.SET_NETWORK_STATUS, {
     network: networkName,
@@ -96,7 +98,7 @@ function connectToApi(network: Network, apiOptions: ApiOptions, _node?: Node): v
   });
 
   const provider = new WsProvider(node.url, AUTO_CONNECT_MS);
-  const api = new ApiPromise({ provider });
+  const api = new ApiPromise({ provider, registry });
 
   apiOptions.api = api;
   apiOptions.provider = provider;

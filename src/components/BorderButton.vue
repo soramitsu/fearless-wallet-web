e2d3939836c0f84d1f24a889f020ee25eb6d91fb
Евгений Галимов <egalimov95@gmail.com>
2022-12-01 20:47:48 +0600

    fix border button

--- src/components/BorderButton.vue
+++ src/components/BorderButton.vue
@@ -1,7 +1,7 @@
 <template>
   <Corners :hover="hover" :size="size" :topLeftCorner="topLeftCorner" :bottomRightCorner="bottomRightCorner">
     <Button
-      type="secondary"
+      :type="type"
       :border="true"
       :borderRadius="borderRadius"
       :disabled="disabled"
@@ -35,6 +35,7 @@ type FontSize = 'small' | 'medium' | 'big';
 export default class BorderButton extends Vue {
   @Prop({ default: '' }) text!: ComponentText;
   @Prop(String) width!: string;
+  @Prop({ default: 'secondary', type: String }) type!: string;
   @Prop(String) iconName!: string;
   @Prop(String) iconColor!: string;
   @Prop({ default: 'medium' }) size!: Size;

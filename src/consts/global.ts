f67dd06c12f10737a731650fde48bba116776a0b
Maksim Baranov <maks.baranow2011@yandex.ru>
2022-12-02 18:50:25 +0300

    update assets price

--- src/consts/global.ts
+++ src/consts/global.ts
@@ -4,3 +4,4 @@ export const APP_NAME = 'Fearless Wallet';
 export const COPYRIGHT = 'Copyright 2022-2023';
 export const AUTHOR = 'Soramitsu';
 export const AUTHOR_WEBSITE = '';
+export const AUTO_UPDATE_ASSETS_PRICE_MS = 1000 * 60 * 5;

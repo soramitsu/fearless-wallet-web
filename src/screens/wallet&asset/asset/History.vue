903e6481a96efd4e804ea1c51dfb1854dba32f48
Евгений Галимов <egalimov95@gmail.com>
2022-12-14 22:41:34 +0600

    minor fix

--- src/screens/wallet&asset/asset/History.vue
+++ src/screens/wallet&asset/asset/History.vue
@@ -11,8 +11,8 @@
         <div :class="classes">
           <template>
             <HistoryItem
-              v-for="historyNode in filteredHistory"
-              :key="historyNode.id"
+              v-for="(historyNode, index) in filteredHistory"
+              :key="index"
               :historyNode="historyNode"
               :assetId="currency.assetId"
               :relayChain="currency.relayChain"

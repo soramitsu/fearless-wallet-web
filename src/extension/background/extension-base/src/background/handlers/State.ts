d517cc2dc4c1759fc3ceee2b488752ed7d756cfe
Евгений Галимов <egalimov95@gmail.com>
2023-02-10 01:12:39 +0600

    revert browser api for create window

--- src/extension/background/extension-base/src/background/handlers/State.ts
+++ src/extension/background/extension-base/src/background/handlers/State.ts
@@ -154,23 +154,22 @@ export default class State {
     const { notification, windows } = await State.getFromStorage(['notification', 'windows']);
 
     if (notification && notification !== 'extension') {
-      const win = await chrome.windows.getCurrent();
-      const popupOptions = { ...POPUP_WINDOW_OPTS };
+      chrome.windows.getCurrent((win) => {
+        const popupOptions = { ...POPUP_WINDOW_OPTS };
 
-      if (win) {
-        popupOptions.left = (win.left || 0) + (win.width || 0) - (POPUP_WINDOW_OPTS.width || 0) - 20;
-        popupOptions.top = (win.top || 0) + 75;
-      }
+        if (win) {
+          popupOptions.left = (win.left || 0) + (win.width || 0) - (POPUP_WINDOW_OPTS.width || 0) - 20;
+          popupOptions.top = (win.top || 0) + 75;
+        }
 
-      chrome.windows
-        .create(notification === 'window' ? NORMAL_WINDOW_OPTS : popupOptions)
-        .then(async (window): Promise<void> => {
+        chrome.windows.create(popupOptions, (window): void => {
           if (window) {
             windows.push(window.id || 0);
 
-            await storage.set({ windows });
+            storage.set({ windows });
           }
         });
+      });
     }
   }
 

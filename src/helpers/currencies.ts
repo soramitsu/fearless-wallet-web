a282830b880fa3c19fd95a5290691a8d9746c877
Maksim Baranov <maks.baranow2011@yandex.ru>
2023-03-01 15:53:36 +0300

    fix

--- src/helpers/currencies.ts
+++ src/helpers/currencies.ts
@@ -166,7 +166,7 @@ function getUtilityAsset(currencies: Currencies, _network: NetworkName): string 
 }
 
 function statusLogging(callback: () => void) {
-  return (result: any) => {
+  return (result: ISubmittableResult) => {
     const { status } = result;
 
     if (status.isBroadcast) {

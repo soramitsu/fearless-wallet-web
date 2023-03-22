48cc5a8825711b70421b2af3f1595e2cb2b23fc8
Евгений Галимов <egalimov95@gmail.com>
2023-03-22 12:37:50 +0600

    update polkadot api version, remove unused packages

--- src/extension/background/extension-base/src/api/tokens/wasm/index.ts
+++
@@ -1,7 +0,0 @@
-import { ApiPromise } from '@polkadot/api';
-import { ContractPromise } from '@polkadot/api-contract';
-import { PSP22Contract } from './helper';
-
-export function getPSP22ContractPromise(apiPromise: ApiPromise, contractAddress: string) {
-  return new ContractPromise(apiPromise, PSP22Contract, contractAddress);
-}

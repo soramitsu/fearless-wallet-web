5aac0c693f07b94e58cc1aa7e286e60d88e308be
Maksim Baranov <maks.baranow2011@yandex.ru>
2023-03-27 16:22:22 +0300

    FWW-397

--- src/screens/wallet&asset/swap/SwapInfo.vue
+++ src/screens/wallet&asset/swap/SwapInfo.vue
@@ -14,28 +14,30 @@
       <div class="value">{{ slippage }}%</div>
     </div>
 
-    <div v-if="showSwapInfo" class="row">
-      <div class="label">
-        {{ $t(minMaxLabel) }}
+    <template v-if="showSwapInfo">
+      <div class="row">
+        <div class="label">
+          {{ $t(minMaxLabel) }}
 
-        <Icon icon="info" class="icon-info min-max" />
+          <Icon icon="info" class="icon-info min-max" />
+        </div>
+
+        <div class="value">
+          <div>{{ minMaxAmount }}</div>
+          <div class="price">{{ minMaxAmountPrice }}</div>
+        </div>
       </div>
 
-      <div class="value">
-        <div>{{ minMaxAmount }}</div>
-        <div class="price">{{ minMaxAmountPrice }}</div>
+      <div class="row">
+        <div class="label">
+          {{ $t('assets.liquidityProvideFee') }}
+
+          <Icon icon="info" class="icon-info provider-fee" />
+        </div>
+
+        <div class="value">{{ providerFeeCut }} {{ soraMainAsset }}</div>
       </div>
-    </div>
-
-    <div v-if="showSwapInfo" class="row">
-      <div class="label">
-        {{ $t('assets.liquidityProvideFee') }}
-
-        <Icon icon="info" class="icon-info provider-fee" />
-      </div>
-
-      <div class="value">{{ providerFeeCut }} {{ soraMainAsset }}</div>
-    </div>
+    </template>
 
     <div class="row">
       <div class="label">
@@ -52,8 +54,8 @@
       <div v-else>-</div>
     </div>
 
-    <Tooltip text="assets.minMaxReceiveInfo" target=".min-max" placement="right" />
-    <Tooltip text="assets.liquidityProvideFeeInfo" target=".provider-fee" placement="right" />
+    <Tooltip v-if="showSwapInfo" text="assets.minMaxReceiveInfo" target=".min-max" placement="right" />
+    <Tooltip v-if="showSwapInfo" text="assets.liquidityProvideFeeInfo" target=".provider-fee" placement="right" />
     <Tooltip text="assets.networkFeeInfo" target=".network-fee" placement="right" />
   </div>
 </template>

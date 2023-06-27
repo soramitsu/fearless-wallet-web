4e89cc3c6d2b67056c9c0e3986ee1b4bc7fb02e5
Евгений Галимов <egalimov95@gmail.com>
2023-06-28 00:27:07 +0600

    setting correct account on update

--- src/extension/background/extension-base/src/background/handlers/Extension.ts
+++ src/extension/background/extension-base/src/background/handlers/Extension.ts
@@ -453,7 +453,7 @@ export default class Extension extends FWExtensionBase {
 
     const accountInfo: CurrentAccountInfo = {
       address,
-      isMobile: (isMobile as boolean) ?? false,
+      isMobile: !!(isMobile as boolean),
       name: name as string,
       ethereumAddress: (ethereumAddress as string) ?? '',
     };

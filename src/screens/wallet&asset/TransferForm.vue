5ba46c8302b67168c089b72c87b1da66f6e64841
Maksim Baranov <maks.baranow2011@yandex.ru>
2023-07-12 16:11:44 +0300

    clear address

--- src/screens/wallet&asset/TransferForm.vue
+++ src/screens/wallet&asset/TransferForm.vue
@@ -565,6 +565,13 @@ export default class TransferForm extends Vue {
     if (this.isTransfer) this.syncedNetwork = this.optionsNetworks?.[0]?.value ?? '';
   }
 
+  @Watch('syncedDestNet')
+  async cleatRecipient() {
+    this.$nextTick(() => {
+      if (!this.isValidRecipientAddress) this.setRecipient();
+    });
+  }
+
   @Watch('syncedAssetId')
   @Watch('syncedNetwork')
   @Watch('syncedDestNet')

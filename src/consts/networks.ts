b57d85daf85e04877b6ac9735fcaa4cc2efc6615
Евгений Галимов <egalimov95@gmail.com>
2023-07-18 19:39:42 +0600

    add fixes for bsc

--- src/consts/networks.ts
+++ src/consts/networks.ts
@@ -3,7 +3,13 @@ import { NetworkName } from '@/interfaces';
 
 const NOT_SUPPORTED_ALL_TRANSFER_NETWORKS = ['karura', 'acala', 'acala_testnet'];
 const SUBSTRATE_ETHEREUM_NETWORKS = ['moonbeam', 'moonriver', 'moonbase alpha', 'astarEvm', 'shidenEvm'];
-const ETHEREUM_NETWORKS = [...SUBSTRATE_ETHEREUM_NETWORKS, 'ethereum', 'ethereum goerli'];
+const ETHEREUM_NETWORKS = [
+  ...SUBSTRATE_ETHEREUM_NETWORKS,
+  'ethereum',
+  'ethereum goerli',
+  'binance smart chain',
+  'binance smart chain testnet',
+];
 const RELAY_CHAINS = ['polkadot', 'kusama', 'westend', 'rococo'];
 const NATIVE_PARACHAINS = [
   'statemint',

32009650d34d7529e04f2973edd5f7ede77b41ab
Евгений Галимов <egalimov95@gmail.com>
2023-07-19 18:39:20 +0600

    fix nonce

--- src/extension/background/extension-base/src/background/handlers/Extension.ts
+++ src/extension/background/extension-base/src/background/handlers/Extension.ts
@@ -1014,7 +1014,6 @@ export default class Extension extends FWExtensionBase {
     const [errors, , tokenInfo] = this.validateTransfer(assetId, from, password);
     const warnings: BasicTxWarning[] = [];
     const isMainToken = checkMainToken(networkKey, tokenInfo.id);
-    const isFromEthereum = isEthereumAddress(from);
 
     const address = getSubstrateAddressByEthAddress(from);
     let fee = 0;

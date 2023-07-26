e16b77c7aa76aead3c37f2d00740351e02cf069a
Евгений Галимов <egalimov95@gmail.com>
2023-07-26 20:08:15 +0600

    fix asset content form height

--- src/consts/global.ts
+++ src/consts/global.ts
@@ -12,7 +12,7 @@ const RAMP_API_KEY = IS_PRODUCTION ? process.env.RAMP_PROD_API_KEY : process.env
 const MOONPAY_API_KEY = IS_PRODUCTION ? process.env.MOONPAY_PROD_API_KEY : process.env.MOONPAY_TEST_API_KEY;
 const IS_EXTENSION = chrome.extension !== undefined;
 const APP_VERSION = packages.version;
-
+const ONE_WEEK = 604800000;
 const SORA_CARD_VISIBILITY = false; // TODO перейти на удаленный JSON
 
 export {
@@ -29,4 +29,5 @@ export {
   IS_EXTENSION,
   APP_VERSION,
   SORA_CARD_VISIBILITY,
+  ONE_WEEK,
 };

7db763270265a52dba553d49c8485af7699c1991
Maksim Baranov <maks.baranow2011@yandex.ru>
2023-08-02 14:41:45 +0300

    link

---
+++ src/components/Link.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="link" @click="$emit('click')">
+    {{ $t(text) }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Link extends Vue {
+  @Prop({ default: 0 }) text!: number;
+}
+</script>
+
+<style lang="scss" scoped>
+.link {
+  color: $pink-lavender-color;
+  text-decoration: underline;
+  text-align: left;
+  cursor: pointer;
+}
+</style>

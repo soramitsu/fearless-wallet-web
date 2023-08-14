64000cccde5c0246d8a2f316a6bc5cc9458fbb67
Евгений Галимов <egalimov95@gmail.com>
2023-08-14 16:07:20 +0600

    add wallet connect dep

--- src/extension/background/extension-base/src/services/wallet-connect-service/index.ts
+++ src/extension/background/extension-base/src/services/wallet-connect-service/index.ts
@@ -1,13 +1,25 @@
 import State from '@extension-base/background/handlers/State';
+import { Core } from '@walletconnect/core';
+import WalletConnectStorage from './storage';
 
 export default class WalletConnectService {
   readonly state: State;
 
+  private client: any | undefined;
+
   constructor(state: State) {
     this.state = state;
+
+    this.initClient();
   }
 
   public addConnection(uri: string) {
     console.info(uri);
+  }
+
+  public async initClient() {
+    this.client = await Core.init({
+      storage: new WalletConnectStorage(),
+    });
   }
 }

6f141ed16b10fbc2c103516fdd7e7405859e3a73
Евгений Галимов <egalimov95@gmail.com>
2023-08-31 12:53:05 +0600

    minor fix

--- src/screens/walletConnect/WalletConnectInit.vue
+++ src/screens/walletConnect/WalletConnectInit.vue
@@ -1,5 +1,5 @@
 <template>
-  <AboveForm :fullScreen="true" :showBackIcon="true" @closeHandler="onBack" @handlerBack="onBack">
+  <AboveForm :fullScreen="true" :showBackIcon="false" @closeHandler="onBack">
     <div class="wc-init-form">
       <FInput v-model="uri" :placeholder="placeholder" size="big" />
 

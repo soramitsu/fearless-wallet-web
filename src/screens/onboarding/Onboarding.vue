17db92715c954f415ab88393978dd6f363611cf0
Evgenii <egalimov95@gmail.com>
2023-09-06 10:00:13 +0600

    add Unbounded-bold font (#468)
    * add Unbounded-bold font
    
    * revert

--- src/screens/onboarding/Onboarding.vue
+++ src/screens/onboarding/Onboarding.vue
@@ -99,6 +99,10 @@ export default class Onboarding extends Vue {
 </script>
 
 <style lang="scss" scoped>
+@font-face {
+  font-family: 'Unbounded';
+  src: local('Unbounded'), url('@/assets/fonts/Unbounded-Bold.ttf') format('truetype');
+}
 .onboarding {
   display: flex;
   flex-flow: column;
@@ -115,6 +119,7 @@ export default class Onboarding extends Vue {
     font-size: 46px;
     font-weight: 700;
     letter-spacing: 0.54px;
+    font-family: 'Unbounded', sans-serif;
     margin: 0;
 
     &--red {

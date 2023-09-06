c1482549dc159d0df5e90213e1b5be7f02ca0354
Maksim Baranov <maks.baranow2011@yandex.ru>
2023-09-06 15:06:37 +0300

    update develop mode

--- src/consts/global.ts
+++ src/consts/global.ts
@@ -8,6 +8,7 @@ const AUTHOR = 'Soramitsu';
 const AUTHOR_WEBSITE = '';
 const AUTO_UPDATE_ASSETS_PRICE_MS = 1000 * 60 * 5;
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const ONLY_SORA = false; // only for testing functionality
 const RAMP_API_KEY = IS_PRODUCTION ? process.env.RAMP_PROD_API_KEY : process.env.RAMP_TEST_API_KEY;
 const MOONPAY_API_KEY = IS_PRODUCTION ? process.env.MOONPAY_PROD_API_KEY : process.env.MOONPAY_TEST_API_KEY;
 const IS_EXTENSION = chrome.extension !== undefined;
@@ -28,4 +29,5 @@ export {
   IS_EXTENSION,
   APP_VERSION,
   ONE_WEEK,
+  ONLY_SORA,
 };

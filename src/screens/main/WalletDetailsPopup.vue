25a2528daea3b6a7061bc336a34477c5ef0bab6f
Maksim Baranov <33489381+MaksimBaranov98@users.noreply.github.com>
2023-09-07 15:21:56 +0300

    Messaing refactoring (#478)
    * fix bugs and refactoring
    
    * messaging refactoring
    
    * fix comments

--- src/screens/main/WalletDetailsPopup.vue
+++ src/screens/main/WalletDetailsPopup.vue
@@ -63,6 +63,7 @@ export default class WalletDetailsPopup extends Vue {
 
   async deleteWallet() {
     forgetAccount(this.selectedWalletAddress, this.isMobileWallet ? 'mobile' : 'native');
+
     if (this.isMobileWallet) beaconController.resetConnection();
     if (this.accounts.length === 0) this.$router.push({ name: Components.Welcome });
     else this.close();

c5967165ba6a82eb9df538a8dcb11e61c0dcf454
Евгений Галимов <egalimov95@gmail.com>
2023-09-08 15:28:35 +0600

    rename file

--- src/extension/messaging/index.ts
+++ src/extension/messaging/index.ts
@@ -90,4 +90,4 @@ export * from '@/extension/messaging/google';
 export * from '@/extension/messaging/networks';
 export * from '@/extension/messaging/balance';
 export * from '@/extension/messaging/common';
-export * from '@/extension/messaging/walletConnect';
+export * from '@/extension/messaging/wallet-connect-requests';

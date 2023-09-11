66025e10c1129a3527379b1663ad7dea0a8d05be
Evgenii <egalimov95@gmail.com>
2023-09-11 23:28:13 +0600

    Bugfix/fww 640 (#489)

---
+++ src/components/Identicon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-if="isEthereumAddress(address)" v-html="getJdenticon(address)"></div>
+
+    <Ident v-else :size="24" theme="polkadot" :value="address" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { toSvg } from 'jdenticon';
+import { Identicon as Ident } from '@polkadot/vue-identicon';
+import BaseApi from '@/util/BaseApi';
+
+defineProps<{ address: string }>();
+
+const getJdenticon = (address: string) => toSvg(address, 24);
+const isEthereumAddress = (address: string) => BaseApi.isEthereumAddress(address);
+</script>

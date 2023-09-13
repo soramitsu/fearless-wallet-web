9cd42c67ef0d31f65a6d9caa7c5ea6e64f0f15ca
Евгений Галимов <egalimov95@gmail.com>
2023-09-13 18:16:13 +0600

    throw error if fetch balance failed

--- src/extension/background/extension-base/src/api/evm/index.ts
+++ src/extension/background/extension-base/src/api/evm/index.ts
@@ -1,7 +1,16 @@
 import { JsonRpcProvider, WebSocketProvider } from 'ethers';
+import { EvmProvider } from '@extension-base/background/types/types';
 
-export const initWeb3Api = (url: string): JsonRpcProvider | WebSocketProvider => {
-  if (url.startsWith('http')) return new JsonRpcProvider(url);
+const initListeners = (provider: EvmProvider) => {
+  provider.on('error', () => {
+    provider.removeAllListeners();
+    provider.destroy();
+  });
+};
 
-  return new WebSocketProvider(url);
+export const initWeb3Api = (url: string): EvmProvider => {
+  const provider = url.startsWith('http') ? new JsonRpcProvider(url) : new WebSocketProvider(url);
+  initListeners(provider);
+
+  return provider;
 };

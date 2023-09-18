bc1c346b17408e038255aa786dd65b88dea0cbe3
Evgenii <egalimov95@gmail.com>
2023-09-18 19:16:22 +0600

    Update/json v3 (#510)
    * Update Cross Chain (#500)
    
    * refactoring
    
    * fixes
    
    * update crossChain
    
    * fixes
    
    * refactoring (#504)
    
    * [DOPS-2723] add api keys for different networks  (#506)
    
    * add api keys
    
    * add alchemy key
    
    * migrate to v3
    
    ---------
    
    Co-authored-by: Maksim Baranov <33489381+MaksimBaranov98@users.noreply.github.com>
    Co-authored-by: alexkg <93205484+alexkg@users.noreply.github.com>

--- src/extension/background/extension-base/src/api/evm/index.ts
+++ src/extension/background/extension-base/src/api/evm/index.ts
@@ -12,7 +12,6 @@ const initListeners = (provider: EvmProvider) => {
 export const initWeb3Api = (url: string): EvmProvider => {
   const apiKey = getEvmApiKey(url);
   const providerUrl = `${url}${apiKey ?? ''}`;
-
   const provider = url.startsWith('http') ? new JsonRpcProvider(providerUrl) : new WebSocketProvider(providerUrl);
 
   initListeners(provider);
--- src/extension/background/extension-base/src/const/networks.ts
+++ src/extension/background/extension-base/src/const/networks.ts
@@ -7,7 +7,7 @@ export const getEvmApiKey = (url: string) => {
   const keys: Record<string, string> = {
     'polygon-mainnet.blastapi.io': process.env.FL_BLAST_API_POLYGON_KEY,
     'polygon-testnet.blastapi.io': process.env.FL_BLAST_API_POLYGON_KEY,
-    'bsc-mainnet.blastapi.io': process.env.FL_BLAST_API_BSC_KEY,
+    'bsc-mainnet.blastapi.io': process.env.FL_BLAST_API_POLYGON_KEY,
     'bsc-testnet.blastapi.io': process.env.FL_BLAST_API_BSC_KEY,
     'eth-mainnet.blastapi.io': process.env.FL_BLAST_API_ETHEREUM_KEY,
     'eth-goerli.blastapi.io': process.env.FL_BLAST_API_GOERLI_KEY,

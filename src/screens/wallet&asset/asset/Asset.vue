8423d2bff46f8337438d87da20aea8670272468f
Евгений Галимов <egalimov95@gmail.com>
2023-09-28 00:27:02 +0600

    fix asset page

--- src/screens/wallet&asset/asset/Asset.vue
+++ src/screens/wallet&asset/asset/Asset.vue
@@ -139,7 +139,12 @@ export default class Asset extends Vue {
   }
 
   get currentCurrency() {
-    return this.balances.find(({ assetId: id }) => id === this.selectedAssetId)! ?? {};
+    return (
+      this.balances.find(
+        ({ assetId: id, balances }) =>
+          id === this.selectedAssetId || balances.some((el) => el.id === this.selectedAssetId)
+      )! ?? {}
+    );
   }
 
   get displayAddressByNetwork() {

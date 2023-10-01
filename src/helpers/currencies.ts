c8b50371862b4105a9a710e6ea963273b0b768cd
Maksim Baranov <maks.baranow2011@yandex.ru>
2023-10-01 03:55:32 +0300

    fix sort assets

--- src/helpers/currencies.ts
+++ src/helpers/currencies.ts
@@ -21,15 +21,25 @@ import { getSummaryTransferableBalance, getTransferableBalanceInNetwork, isNetwo
 function defaultSortingCurrencies(currencies: TokenBalance[], { tokenPriceMap }: AssetsPrice, network: NetworkName) {
   const relayChains = [];
 
-  const currenciesWithAssetsAndWithFiatBalance = currencies.filter(
+  const currenciesThatReady = currencies.filter(({ balances }) =>
+    balances.some(({ state }) => state === APIItemState.READY)
+  );
+
+  const currenciesPending = currencies.filter(({ balances }) =>
+    balances.every(({ state }) => state === APIItemState.PENDING)
+  );
+
+  const currenciesWithAssetsAndWithFiatBalance = currenciesThatReady.filter(
     ({ balances, priceId }) => balances.some(({ total }) => total !== '0') && tokenPriceMap[priceId ?? ''] !== 0
   );
 
-  const currenciesWithAssetsAndWithoutFiatBalance = currencies.filter(
+  const currenciesWithAssetsAndWithoutFiatBalance = currenciesThatReady.filter(
     ({ balances, priceId }) => balances.some(({ total }) => total !== '0') && tokenPriceMap[priceId ?? ''] === 0
   );
 
-  const currenciesWithoutAssets = currencies.filter(({ balances }) => balances.every(({ total }) => total === '0'));
+  const currenciesWithoutAssets = currenciesThatReady.filter(({ balances }) =>
+    balances.every(({ total }) => total === '0')
+  );
 
   const dotIndex = currenciesWithoutAssets.findIndex(({ symbol }) => symbol === 'dot');
 
@@ -71,6 +81,7 @@ function defaultSortingCurrencies(currencies: TokenBalance[], { tokenPriceMap }:
     ...currenciesWithAssetsAndWithoutFiatBalance,
     ...relayChains,
     ...currenciesWithoutAssets,
+    ...currenciesPending,
   ];
 }
 

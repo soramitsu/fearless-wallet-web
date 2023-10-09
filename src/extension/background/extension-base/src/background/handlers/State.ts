c2954e8e2d87aff767c4711f56385157f58beee8
Maksim Baranov <maks.baranow2011@yandex.ru>
2023-10-09 12:42:02 +0300

    minor fix

--- src/extension/background/extension-base/src/background/handlers/State.ts
+++ src/extension/background/extension-base/src/background/handlers/State.ts
@@ -1108,8 +1108,7 @@ export default class State {
     const { data: xcmLocations } = await axios.get<XcmLocations>(URLS.XCM_LOCATIONS);
     const { data: xcmFees } = await axios.get<XcmFees>(URLS.XCM_FEES);
 
-    // TODO REMOVE
-    this.networksJson = networks;
+    this.networksJson = networks.filter((el) => !el.disabled);
     this.xcmLocations = xcmLocations;
     this.xcmFees = xcmFees;
 

a59de1f1c2406ad03be9d9e33065dcc661fc926e
Evgenii <egalimov95@gmail.com>
2023-10-13 16:21:17 +0200

    fix props (#552)

--- src/components/ExternalLogo.vue
+++ src/components/ExternalLogo.vue
@@ -9,7 +9,7 @@ import { NetworksController } from '@/controllers';
 type Props = {
   name: string;
   alt?: string;
-  width: number;
+  width?: number;
 };
 
 const props = withDefaults(defineProps<Props>(), {

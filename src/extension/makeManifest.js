fad1cdbd286e8534ad0249dc9ad60029072d3b21
Евгений Галимов <egalimov95@gmail.com>
2023-10-18 12:45:35 +0200

    move firefox to mv3

--- src/extension/makeManifest.js
+++ src/extension/makeManifest.js
@@ -2,52 +2,30 @@ const { env } = require('process');
 const packageJson = require('../../package.json');
 
 const makeManifest = (browser) => {
-  const baseContentSecurityPolicy =
-    "default-src 'self'; script-src 'self' 'wasm-unsafe-eval'; object-src 'self';  style-src 'unsafe-inline';img-src 'self' https://raw.githubusercontent.com https://cdn.elearningindustry.com data:; frame-src https://soracard.com/; connect-src https: wss: ws:";
-
   const firefoxBase = {
-    manifest_version: 2,
-    permissions: ['storage', 'tabs', 'identity', '*://www.googleapis.com/*', 'clipboardRead'],
-    background: {
-      scripts: ['background.js'],
-      persistent: true,
-    },
     browser_specific_settings: {
       gecko: {
         id: '{6a9332b9-e864-4d0a-a591-140fe75a29ba}',
       },
     },
-    browser_action: {
-      default_title: 'Fearless Wallet',
-      default_popup: 'popup.html#/',
-    },
-    web_accessible_resources: ['page.js'],
-    content_security_policy: baseContentSecurityPolicy,
   };
 
   const chromiumBase = {
-    manifest_version: 3,
-    permissions: ['storage', 'tabs', 'identity', 'clipboardRead'],
-    background: {
-      service_worker: 'background.js',
-      type: 'module',
-    },
-    action: {
-      default_title: 'Fearless Wallet',
-      default_popup: 'popup.html#/',
-    },
     key: env.EXTENSION_PUBLIC_KEY.replace(/ /g, ''),
     oauth2: {
       scopes: ['https://www.googleapis.com/auth/drive.appdata'],
       client_id: env.OAUTH_CLIENT_ID,
     },
     minimum_chrome_version: '92',
-    content_security_policy: {
-      extension_pages: baseContentSecurityPolicy,
-    },
   };
 
   return {
+    manifest_version: 3,
+    permissions: ['storage', 'tabs', 'identity', 'clipboardRead'],
+    background: {
+      service_worker: 'background.js',
+      type: 'module',
+    },
     version: packageJson.version,
     description: packageJson.description,
     homepage_url: 'https://fearlesswallet.io/',
@@ -69,11 +47,18 @@ const makeManifest = (browser) => {
         run_at: 'document_start',
       },
     ],
+    action: {
+      default_title: 'Fearless Wallet',
+      default_popup: 'popup.html#/',
+    },
+    content_security_policy: {
+      extension_pages:
+        "default-src 'self'; script-src 'self' 'wasm-unsafe-eval'; object-src 'self';  style-src 'unsafe-inline';img-src 'self' https://raw.githubusercontent.com https://cdn.elearningindustry.com data:; frame-src https://soracard.com/; connect-src https: wss: ws:",
+    },
     web_accessible_resources: [
       {
         resources: ['page.js'],
         matches: ['https://*/*'],
-        use_dynamic_url: true,
       },
     ],
     ...(browser === 'chrome' ? chromiumBase : firefoxBase),

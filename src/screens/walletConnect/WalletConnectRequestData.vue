738a7cd14a3b2f58e832674d3d84d821685b5476
Евгений Галимов <egalimov95@gmail.com>
2023-10-24 16:09:13 +0200

    fix button text

--- src/screens/walletConnect/WalletConnectRequestData.vue
+++ src/screens/walletConnect/WalletConnectRequestData.vue
@@ -24,12 +24,13 @@ import {
   type WalletConnectTransactionRequest,
 } from '@extension-base/services/wallet-connect-service/types';
 import { formatEther } from 'ethers';
+import { useI18n } from 'vue-i18n-composable';
 import type { AccountJson } from '@extension-base/background/types';
 import { useStore } from '@/store';
 import { cut } from '@/helpers';
 
 const store = useStore();
-
+const { t } = useI18n();
 const props = defineProps<{ request: WalletConnectTransactionRequest }>();
 
 const params = props.request.params.request.params[0] as Record<string, string>;
@@ -44,8 +45,8 @@ const requestData = computed(() => {
   const baseKey = 'walletConnect.requestFields';
 
   if (isSignatureRequest.value) {
-    data[`${baseKey}.method`] = 'walletConnect.personalSign';
-    data[`${baseKey}.message`] = params.data as string;
+    data[`${baseKey}.method`] = t('walletConnect.personalSign').toString();
+    data[`${baseKey}.message`] = params as unknown as string;
 
     return data;
   }
@@ -57,7 +58,7 @@ const requestData = computed(() => {
 
     data[`${baseKey}.network`] = network;
     data[`${baseKey}.amount`] = formatEther(BigInt(value).toString()).toString();
-    data[`${baseKey}.gasFee`] = formatEther(BigInt(gas).toString()).toString();
+    data[`${baseKey}.gasFee`] = gas ? formatEther(BigInt(gas).toString()).toString() : '';
   }
 
   return data;
@@ -112,6 +113,7 @@ const txWallet = computed(() => {
 .tx-details__row {
   border: 1px solid transparent;
   border-bottom-color: $default-background-color;
+  display: flex;
 }
 .tx-details__list:last-child {
   border-bottom-color: transparent;

943f2ea9c7676898394e77dd0d07f70096c148b6
Евгений Галимов <egalimov95@gmail.com>
2023-10-25 23:11:56 +0200

    fix unsupported required network case

--- src/screens/walletConnect/WalletConnectAuthConfirmation.vue
+++ src/screens/walletConnect/WalletConnectAuthConfirmation.vue
@@ -111,9 +111,9 @@ const namespaces = computed<ChainData[]>(() => {
 
   const requiredNamespaces = request.value.request.params.requiredNamespaces;
   const optionalNamespaces = request.value.request.params.optionalNamespaces;
-  const transformedRequiredNamespaces = transformNamespaces(requiredNamespaces);
-  const transformedOptionalNamespaces = transformNamespaces(optionalNamespaces);
-
+  const transformedRequiredNamespaces = transformNamespaces(requiredNamespaces, true);
+  if (transformedRequiredNamespaces.length === 0) return [];
+  const transformedOptionalNamespaces = transformNamespaces(optionalNamespaces, false);
   const result = [...transformedRequiredNamespaces, ...transformedOptionalNamespaces];
   const arrSet = new Map();
 
@@ -203,10 +203,8 @@ const onReject = () => {
 .auth-confirmation {
   display: flex;
   align-items: center;
-
   justify-content: space-between;
   flex-direction: column;
-  padding: 6px;
   gap: 10px;
 }
 .namespaces-form {

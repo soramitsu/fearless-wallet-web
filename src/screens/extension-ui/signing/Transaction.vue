56453ed70e28798bee3314603154c39e1ec02235
Maksim Baranov <maks.baranow2011@yandex.ru>
2023-10-25 14:20:30 +0300

    update sign transaction , fix bugs

--- src/screens/extension-ui/signing/Transaction.vue
+++ src/screens/extension-ui/signing/Transaction.vue
@@ -22,61 +22,113 @@
           <InfoItem v-for="(value, key) in txInfo" :name="key" :value="value" :key="key" />
         </InfoList>
 
-        <ConfirmationPasswordPopup
-          v-if="isSignPopupVisible"
-          sizeWidth="medium"
-          :address="payload.address"
-          :transactionId="request.id"
-          :payload="payload"
-          @close="onClose"
-        />
+        <template v-if="!isSignMobile">
+          <ValidatedInput
+            ref="passInput"
+            v-model="password"
+            placeholder="common.password"
+            size="big"
+            :class="classesInput"
+            errorDescriptions="common.invalidPassword"
+            :readonly="!isLocked"
+            :isError="isErrorPassword"
+            :showPassword="true"
+            @keypress.native="keypress"
+          />
+
+          <div v-if="isExtension" class="remember-checkbox">
+            <Checkbox v-model="isSavePass" size="medium" :label="$t(min15Label)" />
+          </div>
+        </template>
       </div>
 
-      <FButton size="big" class="button" text="assets.signTransaction" @click="onSign" />
+      <FButton v-if="!isSignMobile" size="big" class="button" text="assets.signTransaction" @click="sendExtrinsic" />
+
+      <SignMobile v-else @onSign="onSignMobile" @onCancel="closeSignMobile" />
     </div>
   </AboveForm>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Component, Vue, Watch, Ref } from 'vue-property-decorator';
 import { Getter, Action } from 'vuex-class';
 import registry from '@extension-base/api/substrate/typeRegistry';
-import type { AccountJson } from '@extension-base/background/types';
+import type { AccountJson, SigningRequest } from '@extension-base/background/types/types';
 import type { ExtrinsicEra } from '@polkadot/types/interfaces';
-import type { SigningRequest } from '@extension-base/background/types/types';
+import type ValidatedInput from '@/components/ValidatedInput.vue';
 import BaseApi from '@/util/BaseApi';
 import Checkbox from '@/components/Checkbox.vue';
 import WalletInfo from '@/screens/extension-ui/signing/WalletInfo.vue';
 import InfoList from '@/screens/extension-ui/InfoList.vue';
 import InfoItem from '@/screens/extension-ui/InfoItem.vue';
-import ConfirmationPasswordPopup from '@/screens/wallet&asset/ConfirmationPasswordPopup.vue';
 import { GettersTypes as ExtensionGettersTypes } from '@/store/extension/getters';
-import { ActionTypes as ExtensionActionTypes } from '@/store/extension/actions';
+import { ActionTypes as ExtensionActionTypes, ApprovePayload } from '@/store/extension/actions';
 import { AsyncFn, SignerPayloadJSON, PayloadJSON } from '@/interfaces';
 import { beaconController, ExtensionController } from '@/controllers';
 import { Components } from '@/router/routes';
 import { GettersTypes as AccountsGettersTypes } from '@/store/accounts/getters';
+import { SelectedWallet } from '@/store';
+import { IS_EXTENSION } from '@/consts/global';
+import { isSignLocked, validatePassword } from '@/extension/messaging';
+import SignMobile from '@/screens/wallet&asset/SignMobile.vue';
 
 @Component({
   components: {
     WalletInfo,
-    ConfirmationPasswordPopup,
     InfoItem,
     InfoList,
     Checkbox,
+    SignMobile,
   },
 })
-export default class Auth extends Vue {
-  isLocked = false;
+export default class Transaction extends Vue {
+  readonly isExtension = IS_EXTENSION;
+  isLocked = true;
   isSignPopupVisible = false;
+  isErrorPassword = false;
+  password = '';
+  isSavePass = false;
 
+  @Ref('passInput') readonly passInputComponent!: ValidatedInput;
   @Getter(ExtensionGettersTypes.signRequestPayload) payload!: SignerPayloadJSON;
   @Getter(ExtensionGettersTypes.signList) requests!: SigningRequest[];
   @Action(ExtensionActionTypes.SIGN_CANCEL) onSignCancel!: AsyncFn<string>;
   @Getter(AccountsGettersTypes.getAccounts) accounts!: AccountJson[];
+  @Getter(AccountsGettersTypes.selectedWallet) selectedWallet!: SelectedWallet;
+  @Action(ExtensionActionTypes.APPROVE_SIGN_PASSWORD) onSignApprove!: AsyncFn<ApprovePayload>;
+
+  get classesInput() {
+    return [
+      'row',
+      'password-input',
+      {
+        'password-input-margin': !this.isExtension,
+      },
+    ];
+  }
+
+  get transactionId() {
+    return this.request.id;
+  }
+
+  get disabledButton() {
+    if (!this.isLocked) return false;
+
+    return this.password === '' || this.isErrorPassword;
+  }
+
+  get transactionAddress() {
+    return this.payload?.address ?? this.selectedWallet.address;
+  }
 
   get request() {
     return this.requests[0];
+  }
+
+  get isSignMobile() {
+    const encodedAddress = BaseApi.encodeAddress(this.transactionAddress);
+
+    return this.accounts.some((account) => account.address === encodedAddress && account.isMobile);
   }
 
   get address() {
@@ -132,6 +184,10 @@ export default class Auth extends Vue {
     return this.mortalityAsString(this.typedPayload.era, this.payload.blockNumber);
   }
 
+  get min15Label() {
+    return this.isLocked ? 'assets.15min' : 'assets.15minExtend';
+  }
+
   async mounted() {
     if (this.isMobileSignRequired) {
       const payload: PayloadJSON = this.payload;
@@ -146,6 +202,22 @@ export default class Auth extends Vue {
 
       this.$router.push({ name: Components.Wallet });
     }
+
+    if (!IS_EXTENSION || this.isSignMobile) return;
+
+    this.passInputComponent.input.focus();
+
+    const { isLocked } = await isSignLocked(this.transactionAddress);
+
+    this.isLocked = isLocked;
+    this.isSavePass = !this.isLocked;
+
+    if (!isLocked) this.password = '000000';
+  }
+
+  @Watch('password')
+  async resetStatusError() {
+    this.isErrorPassword = false;
   }
 
   @Watch('requests')
@@ -161,16 +233,56 @@ export default class Auth extends Vue {
     return `mortal, valid from ${birth} to ${death}`;
   }
 
-  onSign() {
-    this.isSignPopupVisible = true;
-  }
-
   onClose() {
     this.isSignPopupVisible = false;
   }
 
   async onReject() {
     this.onSignCancel(this.request.id);
+  }
+
+  async onSignMobile() {
+    this.signTransactionJSON(this.transactionId);
+  }
+
+  async closeSignMobile() {
+    // TODO
+  }
+
+  keypress({ key }: KeyboardEvent) {
+    if (key === 'Enter') this.sendExtrinsic();
+  }
+
+  async signTransactionJSON(id: string) {
+    const payload: PayloadJSON = this.payload as SignerPayloadJSON;
+    delete payload.address;
+    payload.type = 'json';
+
+    const { blockchainData } = await beaconController.sendRequestJSON(payload as unknown as PayloadJSON);
+
+    if (blockchainData.signature.length === 0) {
+      ExtensionController.cancelSign(id);
+
+      return;
+    }
+
+    ExtensionController.approveSignSignature(id, blockchainData.signature);
+  }
+
+  async sendExtrinsic() {
+    const isValidPass = await validatePassword(this.address, this.password);
+
+    if (!isValidPass) {
+      this.isErrorPassword = true;
+
+      return;
+    }
+
+    this.onSignApprove({
+      id: this.transactionId,
+      isSavePass: this.isSavePass,
+      password: this.password,
+    });
   }
 }
 </script>
@@ -193,5 +305,23 @@ export default class Auth extends Vue {
   align-items: center;
   justify-content: space-between;
   flex-flow: column;
+}
+
+.remember-checkbox {
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+}
+
+.row {
+  margin-top: 15px;
+}
+
+.password-input {
+  width: 100%;
+}
+
+.password-input-margin {
+  margin-bottom: 15px;
 }
 </style>

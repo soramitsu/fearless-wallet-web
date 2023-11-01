bf6d3cc7f4ea56edba47991f1dc9506055329c87
Евгений Галимов <egalimov95@gmail.com>
2023-11-01 14:27:08 +0100

    wip

--- src/extension/background/extension-base/src/services/wallet-connect-service/dapp.ts
+++ src/extension/background/extension-base/src/services/wallet-connect-service/dapp.ts
@@ -1,4 +1,5 @@
 import UniversalProvider from '@walletconnect/universal-provider';
+
 import { PROJECT_ID_EXTENSION, WALLET_CONNECT_DAPP_CONFIG, WALLET_CONNECT_METADATA } from './consts';
 import WalletConnectStorage from './storage';
 
@@ -14,21 +15,21 @@ export default class WalletConnectDAppService {
       metadata: WALLET_CONNECT_METADATA,
       storage: new WalletConnectStorage(),
     });
+    this.onSessionUpdate();
   }
 
   async initSession(): Promise<string> {
     if (!this.app) throw new Error('App client not init');
 
     const { uri } = await this.app.client.connect(WALLET_CONNECT_DAPP_CONFIG);
-
     if (!uri) throw new Error('uri is undefined');
 
     return uri;
   }
 
-  // disconnect(topic: string) {
-  // this.app?.client.disconnect({ topic });
-  // }
+  disconnect(topic: string) {
+    this.app?.client.core.pairing.disconnect({ topic });
+  }
 
   // onResponse() {
   //   this.app?.on('auth_response', ({ params }) => {
@@ -40,17 +41,24 @@ export default class WalletConnectDAppService {
   //     }
   //   });
   // }
+
   // onPairingDelete() {
   //   this.app?.client.pairing.core.on('pairing_delete', ({ id, topic }) => {
   //     console.log(id, topic);
   //     // clean up after the pairing for `topic` was deleted.
   //   });
   // }
+
   // onPairingExpire() {
   //   this.app?.client.pairing.core.on('pairing_expire', ({ id, topic }) => {
   //     console.log(id, topic);
   //     // clean up after the pairing for `topic` was deleted.
   //   });
+  // }
+  //   onSessionUpdate() {
+  // this.app?.client.on('session_update', ({ id, topic, params }) => {
+  // console.log(id, topic, params);
+  // });
   // }
   // onRequest() {
   //   this.app?.client.pairing.core.on('auth_request', ({ params }) => {

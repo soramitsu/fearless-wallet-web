5a2022d160cf7550687de61fe25c401b2d2553c3
Евгений Галимов <egalimov95@gmail.com>
2023-11-03 22:59:45 +0100

    add currencies with api error to the list

--- src/helpers/currencies.ts
+++ src/helpers/currencies.ts
@@ -22,6 +22,9 @@ function defaultSortingCurrencies(currencies: TokenBalance[], { tokenPriceMap }:
   const currenciesPending = currencies.filter(({ balances }) =>
     balances.every(({ state }) => state === APIItemState.PENDING)
   );
+  const currenciesWithError = currencies.filter(({ balances }) =>
+    balances.every(({ state }) => state === APIItemState.ERROR)
+  );
 
   const currenciesWithAssetsAndWithFiatBalance = currenciesThatReady.filter(
     ({ balances, priceId }) => balances.some(({ total }) => total !== '0') && tokenPriceMap[priceId ?? ''] !== 0
@@ -76,6 +79,7 @@ function defaultSortingCurrencies(currencies: TokenBalance[], { tokenPriceMap }:
     ...relayChains,
     ...currenciesWithoutAssets,
     ...currenciesPending,
+    ...currenciesWithError,
   ];
 }
 

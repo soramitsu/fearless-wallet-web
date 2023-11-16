5ba74b8decad0a784a8f93035f6fdcb1b4ef864c
Евгений Галимов <egalimov95@gmail.com>
2023-11-16 14:34:58 +0100

    fix price service

--- src/extension/background/extension-base/src/services/prices-service/index.ts
+++ src/extension/background/extension-base/src/services/prices-service/index.ts
@@ -1,6 +1,7 @@
 import PriceStore from '@extension-base/stores/Price';
 import { axios } from '@extension-base/utils';
 import { REFRESH_PRICE_INTERVAL } from '@extension-base/const/intervals';
+import { storage } from '@extension-base/stores/Storage';
 import type State from '@extension-base/background/handlers/State';
 import type { PriceJson } from '@extension-base/background/types/types';
 
@@ -21,35 +22,37 @@ export default class PricesService {
   };
   private priceStoreReady = false;
   public fiatSymbol = 'usd';
-  private readonly priceStore;
+  private readonly priceStore: PriceStore;
   state: State;
+
   constructor(state: State) {
     this.state = state;
     this.priceStore = new PriceStore();
   }
 
-  public refreshPrice() {
-    const assets: string[] = this.state.assetsMap.filter(({ priceId }) => priceId).map(({ priceId }) => priceId);
+  get priceIds() {
+    const assets = this.state.assetsMap.flatMap(({ priceId }) => (priceId ? [priceId] : []));
 
-    this.getTokenPrice(Array.from(new Set(assets)), this.fiatSymbol, this.prices)
-      .then((rs) => {
-        this.setPrice(rs);
-      })
+    return Array.from(new Set(assets));
+  }
+
+  public refreshPrice() {
+    this.getTokenPrice(this.priceIds, this.fiatSymbol, this.prices)
+      .then((rs) => this.setPrice(rs))
       .catch((err) => console.info(err));
   }
+
   public setFiatSymbol(symbol: string) {
     this.fiatSymbol = symbol;
 
-    chrome.storage.local.set({ fiatSymbol: this.fiatSymbol });
+    storage.set({ fiatSymbol: this.fiatSymbol });
   }
 
   public setPrice(priceData: PriceJson, callback?: (priceData: PriceJson) => void): void {
     this.priceStore.set('PriceData', priceData, () => {
-      if (callback) {
-        callback(priceData);
+      if (callback) callback(priceData);
 
-        this.priceStoreReady = true;
-      }
+      this.priceStoreReady = true;
     });
   }
 
@@ -57,9 +60,7 @@ export default class PricesService {
     this.priceStore.get('PriceData', (rs) => {
       if (this.priceStoreReady) update(rs);
       else {
-        const assets: string[] = this.state.assetsMap.filter(({ priceId }) => priceId).map(({ priceId }) => priceId);
-
-        this.getTokenPrice(Array.from(new Set(assets)), this.fiatSymbol, this.prices)
+        this.getTokenPrice(this.priceIds, this.fiatSymbol, this.prices)
           .then((rs) => {
             this.setPrice(rs);
             update(rs);

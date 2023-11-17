99c815e516f032379cb1b4e238831c289d327e8b
Evgenii <egalimov95@gmail.com>
2023-11-17 11:26:13 +0100

    fix apies init (#603)

--- src/extension/background/extension-base/src/background/handlers/State.ts
+++ src/extension/background/extension-base/src/background/handlers/State.ts
@@ -789,17 +789,19 @@ export default class State {
       const { name, isEthereum } = network;
 
       if (isEthereum && isRequireEvmAPI(name)) {
-        if (this.apis.evm[name] || this.apis.evm[name].ready) {
+        if (!this.apis.evm[name] || !this.apis.evm[name].ready) {
           this.initWeb3Api(network);
         }
       } else {
-        if (this.apis.substrate[name]) {
-          this.apis.substrate[name].api?.isReady.catch(() => {
-            this.resetApiRetries();
+        const initSubstrateApies = () => {
+          this.resetApiRetries();
 
-            initApi(network, this);
-          });
-        }
+          initApi(network, this);
+        };
+
+        if (this.apis.substrate[name]) {
+          this.apis.substrate[name].api?.isReady.catch(initSubstrateApies);
+        } else initSubstrateApies();
       }
     });
   }

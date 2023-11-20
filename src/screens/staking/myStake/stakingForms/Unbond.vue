10e0592a5f3240b9d9712fee7ef9d02d7f1310ef
Maksim Baranov <33489381+MaksimBaranov98@users.noreply.github.com>
2023-11-20 16:06:33 +0300

    fix (#611)

--- src/screens/staking/myStake/stakingForms/Unbond.vue
+++ src/screens/staking/myStake/stakingForms/Unbond.vue
@@ -22,7 +22,7 @@
       {{ $t('staking.unstakingDisclaimers2') }}
     </div>
 
-    <Tooltip text="assets.stakingFee" target=".network-fee" placement="right" />
+    <Tooltip text="staking.stakingFee" target=".network-fee" placement="right" />
   </div>
 </template>
 

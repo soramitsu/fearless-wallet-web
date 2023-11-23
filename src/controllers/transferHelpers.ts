77e9aa2d9f5e7eb19ee3d9ac59977b8421658a14
Maksim Baranov <33489381+MaksimBaranov98@users.noreply.github.com>
2023-11-23 12:51:12 +0300

    fix staking price (#628)

--- src/controllers/transferHelpers.ts
+++ src/controllers/transferHelpers.ts
@@ -3,8 +3,16 @@ import { type NetworkName } from '@/interfaces';
 import { type Wallet } from '@/store';
 import BaseApi from '@/util/BaseApi';
 
-export function getCostOfAssets(count: number | string, price: number): number {
-  return new FPNumber(count).mul(new FPNumber(price)).toNumber();
+export function getCostOfAssets(
+  count: number | string,
+  price: number,
+  returnType: 'string' | 'number' = 'number'
+): number | string {
+  const fp = new FPNumber(count).mul(new FPNumber(price));
+
+  if (returnType === 'string') return fp.toString();
+
+  return fp.toNumber();
 }
 
 export function getTransactionAddress(wallet: Wallet, network: NetworkName): string {

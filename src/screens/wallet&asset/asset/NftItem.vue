6e1bcb6b44f289a383b700501484109e29098ba8
Евгений Галимов <egalimov95@gmail.com>
2023-11-23 11:16:45 +0100

    fix styles

--- src/screens/wallet&asset/asset/NftItem.vue
+++ src/screens/wallet&asset/asset/NftItem.vue
@@ -3,10 +3,13 @@
     <div class="nft">
       <img :src="nft.image.cachedUrl" :alt="nft.collection?.name" />
       <div class="nft-info">
-        <!-- <span>{{ upperTitle }}</span> -->
-        <span class="nft-info__title">{{ title }}</span>
-        <!-- <span>{{ subTitle }}</span>
-        <Icon icon="export-nft" width="42" class="nft-info__share" /> -->
+        <div class="titles">
+          <span v-if="isNft" class="title--overflow">{{ upperTitle }}</span>
+          <span class="title--main title--overflow">{{ title }}</span>
+          <span v-if="isNft" class="title--overflow">{{ subTitle }}</span>
+        </div>
+
+        <Icon v-if="isNft" icon="export-nft" width="42px" height="42px" class="share" />
       </div>
     </div>
   </FCorners>
@@ -19,30 +22,61 @@ import type { OwnedNft } from 'alchemy-sdk';
 type Props = {
   nft: OwnedNft;
 };
-
+const isNft = ref(false);
 const props = defineProps<Props>();
 const title = ref(props.nft.collection?.name ?? '');
-// const subTitle = ref('#1 subtitle');
-// const upperTitle = ref('#1 subtitle');
+const subTitle = ref('#1 subtitle');
+const upperTitle = ref('#1 subtitle');
 
 onMounted(() => {});
 </script>
+
 <style lang="scss">
 .nft {
   display: flex;
   width: 239px;
   background: $secondary-background-color;
-  border: none;
   clip-path: $big-clip-path-left-top-and-right-bottom;
   border-radius: $default-border-radius;
   flex-direction: column;
-  overflow: hidden;
 }
 
 .nft-info {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4px;
   padding: 20px;
   text-align: start;
-  font-size: 16px;
-  line-height: 20px;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 15px;
+  color: $grayish-white;
+}
+
+.titles {
+  display: flex;
+  flex-flow: column;
+  gap: 8px;
+  max-width: 150px;
+
+  .title--overflow {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  .title--main {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    color: #ffffff;
+  }
+}
+
+.share {
+  width: 42px;
+  height: 42px;
 }
 </style>

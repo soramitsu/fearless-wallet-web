e98f898c9d5707906143e40b7f531e1a6aebe6bb
Evgenii <egalimov95@gmail.com>
2023-12-19 15:04:47 +0100

    Bugfix/fww 834 1 (#673)
    * add mobile icon to header
    
    * flex align
    
    * fix node bugs
    
    * fix comment

--- src/screens/main/Header.vue
+++ src/screens/main/Header.vue
@@ -16,7 +16,7 @@
       <div class="wallet-name">
         <div class="name" @click.stop="toggleSelectWalletPopupVisible">
           <span class="wallet-title" data-testid="walletNameHeader">{{ name }}</span>
-
+          <Icon v-if="isMobile" icon="mobile" className="mobile" />
           <Rotate :isActive="syncedShowSelectWalletPopup">
             <SIcon name="chevron-bottom-16" />
           </Rotate>
@@ -239,6 +239,10 @@ export default class Header extends Vue {
     return !this.tabStatus || !this.tabStatus.isAuthorize ? 'header.notConnected' : 'header.connected';
   }
 
+  get isMobile() {
+    return !!this.selectedWallet.isMobile;
+  }
+
   copyAddress() {
     navigator.clipboard.writeText(this.address);
   }
@@ -341,6 +345,7 @@ export default class Header extends Vue {
     .wallet-name {
       display: flex;
       align-items: flex-start;
+      justify-content: center;
       flex-direction: column;
 
       .name {
@@ -418,6 +423,10 @@ export default class Header extends Vue {
 
   .fail-connect {
     background-color: $gray-color;
+  }
+  .mobile {
+    width: 18px;
+    height: 18px;
   }
 }
 </style>

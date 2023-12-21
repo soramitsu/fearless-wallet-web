85392080c9d52fe32413a16b392e929f09d5c4e1
Evgenii <egalimov95@gmail.com>
2023-12-21 10:40:10 +0100

    Bugfix/wc dapp proposal (#677)
    * add ability to change mobile wallet name
    
    * fix proposal body

--- src/extension/background/extension-base/src/services/wallet-connect-service/dapp.ts
+++ src/extension/background/extension-base/src/services/wallet-connect-service/dapp.ts
@@ -83,14 +83,14 @@ export default class WalletConnectDAppService {
             'polkadot:91b171bb158e2d3848fa23a9f1c25182', //dot
             'polkadot:7e4e32d0feafd4f9c9414b0be86373f9', //sora mainnet
           ],
-          events: ['chainChanged", "accountsChanged'],
+          events: [],
         },
       },
       optionalNamespaces: {
         polkadot: {
           methods: ['polkadot_signTransaction', 'polkadot_signMessage'],
           chains: optionalChains,
-          events: ['chainChanged", "accountsChanged'],
+          events: [],
         },
       },
     });

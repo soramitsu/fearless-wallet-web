bb8e53a58589f9d2cecef0ec40ea8d67d173210b
Евгений Галимов <egalimov95@gmail.com>
2023-12-28 11:58:17 +0100

    composition api upgrade

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,14 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { Mutation, Getter, Action } from 'vuex-class';
+<script lang="ts" setup>
+import { computed, onMounted, ref, onUnmounted } from 'vue';
+import { useRouter } from 'vue-router/composables';
 import { ALL_NETWORKS } from './consts/networks';
 import { setTitle } from './helpers/common';
-import type { AccountJson, BalanceJson, PriceJson } from '@extension-base/background/types/types';
-import type { SetAccountsProps, SetNetworksStatusProps, SetAssetsPriceProps } from '@/store';
-import type { AsyncFn, Fn } from '@/interfaces';
+import type { AccountJson, PriceJson } from '@extension-base/background/types/types';
+import { useStore } from '@/store';
 import { Components } from '@/router/routes';
-import { ActionTypes as ExtensionActionTypes } from '@/store/extension/actions';
-import { MutationTypes as ExtensionMutationTypes } from '@/store/extension/mutations';
-import { MutationTypes as AccountsMutationTypes } from '@/store/accounts/mutations';
-import { MutationTypes as NetworksMutationTypes } from '@/store/networks/mutations';
-import { ActionTypes as AccountsActionTypes } from '@/store/accounts/actions';
-import { GettersTypes as AccountsGettersTypes } from '@/store/accounts/getters';
 import {
   isOnboardingRequired,
   pingServiceWorker,
@@ -30,120 +23,107 @@ import {
   subscribeNetworkMap,
   subscribePrice,
 } from '@/extension/messaging';
-import { ActionTypes as NetworksActionTypes } from '@/store/networks/actions';
 import { IS_EXTENSION, IS_PRODUCTION } from '@/consts/global';
-import { ActionTypes as SoraCardActionTypes } from '@/store/soraCard/actions';
 
-@Component({})
-export default class App extends Vue {
-  @Getter(AccountsGettersTypes.showPolkaswapAlert) showPolkaswapAlert!: boolean;
-  @Getter(AccountsGettersTypes.getAccounts) wallets!: AccountJson[];
-  @Mutation(NetworksMutationTypes.SET_NETWORKS) setNetworks!: Fn<SetNetworksStatusProps>;
-  @Mutation(NetworksMutationTypes.SET_ASSETS_PRICE) setPrices!: Fn<SetAssetsPriceProps>;
-  @Mutation(AccountsMutationTypes.SET_ACCOUNTS) setAccounts!: Fn<SetAccountsProps>;
-  @Mutation(AccountsMutationTypes.SET_SELECTED_FIAT) setSelectedFiat!: Fn<string>;
-  @Mutation(AccountsMutationTypes.SET_SELECTED_NETWORK) setSelectedNetwork!: (network: string) => void;
-  @Action(NetworksActionTypes.FETCH_FIATS) fetchFiats!: AsyncFn;
-  @Action(SoraCardActionTypes.GET_USER_STATUS) getUserStatus!: AsyncFn;
-  @Action(AccountsActionTypes.SET_SELECTED_WALLET) setSelectedWallet!: AsyncFn<AccountJson>;
-  @Action(AccountsActionTypes.SET_BALANCE) setBalance!: AsyncFn<BalanceJson>;
-  @Action(ExtensionActionTypes.SUBSCRIBE_EXTENSION_REQUESTS) extensionSubscribe!: AsyncFn;
-  @Action(ExtensionActionTypes.FETCH_FEATURES) fetchFeatures!: AsyncFn;
-  @Mutation(ExtensionMutationTypes.SET_ONBOARDING) setOnboarding!: (payload: boolean) => void;
+const store = useStore();
+const router = useRouter();
+const intervalId = ref<NodeJS.Timer | undefined>();
+const showPolkaswapAlert = computed<boolean>(() => store.getters.showPolkaswapAlert);
+const wallets = computed<AccountJson[]>(() => store.getters.getAccounts);
 
-  get includeKeepAlive() {
-    const components = ['Main'];
+const includeKeepAlive = computed(() => {
+  const components = ['Main'];
 
-    if (this.showPolkaswapAlert) components.push('SwapForm');
+  if (showPolkaswapAlert.value) components.push('SwapForm');
 
-    return components;
-  }
+  return components;
+});
 
-  async created() {
-    if (IS_EXTENSION) this.extensionSubscribe();
+const setupSWPing = () => {
+  intervalId.value = setInterval(() => {
+    pingServiceWorker().catch(() => window.close());
+  }, 20000);
+};
 
-    setTitle();
+const setupBalance = async () => {
+  const balance = await subscribeBalance((balanceUpdates) => store.dispatch('SET_BALANCE', balanceUpdates));
 
-    this.setupWallet();
-    this.setupNetworks();
-    this.setupBalance();
-    this.fetchFiats();
-    this.setupPrice();
-    this.setupSWPing();
+  store.dispatch('SET_BALANCE', balance);
+};
 
-    await this.fetchFeatures();
+const setupNetworks = async () => {
+  const nets = await subscribeNetworkMap((networksUpdates) =>
+    store.commit('SET_NETWORKS', { networks: Object.values(networksUpdates) })
+  );
 
-    this.getUserStatus(); // SORA Card
-  }
+  store.commit('SET_NETWORKS', { networks: Object.values(nets) });
+};
 
-  async mounted() {
-    if (IS_PRODUCTION) {
-      const isRequired = await isOnboardingRequired();
+const updatePrice = ({ currency, tokenPriceMap, tokenPriceChange }: PriceJson) => {
+  store.commit('SET_SELECTED_FIAT', currency);
+  store.commit('SET_ASSETS_PRICE', { tokenPriceMap, tokenPriceChange });
+};
 
-      if (isRequired) this.$router.push({ name: Components.Onboarding });
-    }
-  }
+const setupPrice = async () => {
+  const prices = await subscribePrice((priceUpdates) => {
+    updatePrice(priceUpdates);
+  });
 
-  setupSWPing() {
-    setInterval(() => {
-      try {
-        pingServiceWorker();
-      } catch (error) {
-        window.close();
-      }
-    }, 20000);
-  }
+  updatePrice(prices);
+};
 
-  async setupBalance() {
-    const balance = await subscribeBalance((balanceUpdates) => this.setBalance(balanceUpdates));
+const onAccountUpdate = (accounts: AccountJson[], isMobileUpdate = false) => {
+  const selectedAccount = accounts.find((account) => account.active);
 
-    this.setBalance(balance);
-  }
+  store.commit('SET_ACCOUNTS', { accounts, isMobileUpdate });
 
-  async setupNetworks() {
-    const nets = await subscribeNetworkMap((networksUpdates) =>
-      this.setNetworks({ networks: Object.values(networksUpdates) })
+  if (selectedAccount || !wallets.value.length) {
+    store.commit('SET_SELECTED_WALLET', selectedAccount);
+    store.commit(
+      'SET_SELECTED_NETWORK',
+      selectedAccount && selectedAccount.network ? selectedAccount.network : ALL_NETWORKS
     );
-
-    this.setNetworks({ networks: Object.values(nets) });
   }
+};
 
-  async setupPrice() {
-    const prices = await subscribePrice((priceUpdates) => {
-      this.updatePrice(priceUpdates);
-    });
+const setupWallet = async () => {
+  const accounts = await subscribeAccounts((accounts) => {
+    onAccountUpdate(accounts);
+  });
 
-    this.updatePrice(prices);
+  onAccountUpdate([...accounts]);
+
+  subscribeAddresses((accounts) => {
+    onAccountUpdate(accounts, true);
+  });
+};
+
+onMounted(async () => {
+  if (IS_EXTENSION) store.dispatch('SUBSCRIBE_EXTENSION_REQUESTS');
+
+  setTitle();
+
+  setupWallet();
+  setupNetworks();
+  setupBalance();
+  store.dispatch('FETCH_FIATS');
+  setupPrice();
+  setupSWPing();
+
+  await store.dispatch('FETCH_FEATURES');
+
+  store.dispatch('GET_USER_STATUS'); // SORA Card
+
+  if (IS_PRODUCTION) {
+    const isRequired = await isOnboardingRequired();
+
+    if (isRequired) router.push({ name: Components.Onboarding });
   }
+});
 
-  updatePrice({ currency, tokenPriceMap, tokenPriceChange }: PriceJson) {
-    this.setSelectedFiat(currency);
-    this.setPrices({ tokenPriceMap, tokenPriceChange });
-  }
-
-  onAccountUpdate(accounts: AccountJson[], isMobileUpdate = false) {
-    const selectedAccount = accounts.find((account) => account.active);
-
-    this.setAccounts({ accounts, isMobileUpdate });
-
-    if (selectedAccount || !this.wallets.length) {
-      this.setSelectedWallet(selectedAccount);
-      this.setSelectedNetwork(selectedAccount && selectedAccount.network ? selectedAccount.network : ALL_NETWORKS);
-    }
-  }
-
-  async setupWallet() {
-    const accounts = await subscribeAccounts((accounts) => {
-      this.onAccountUpdate(accounts);
-    });
-
-    this.onAccountUpdate([...accounts]);
-
-    subscribeAddresses((accounts) => {
-      this.onAccountUpdate(accounts, true);
-    });
-  }
-}
+onUnmounted(() => {
+  clearInterval(intervalId.value);
+});
 </script>
 
 <style lang="scss">

69ad352274303f4502fbfcf06d0e25b350d4fe19
Евгений Галимов <egalimov95@gmail.com>
2024-01-19 11:55:41 +0100

    refactoring

--- src/screens/wallet&asset/asset/NftSettings.vue
+++ src/screens/wallet&asset/asset/NftSettings.vue
@@ -12,39 +12,34 @@
     <div class="nft-settings">
       <div class="form-item">
         <span>Spam</span>
-        <Switcher v-model="spam" />
+        <Switcher v-model="nftSettings.spam" />
       </div>
       <div class="form-item">
         <span>Airdrops</span>
-        <Switcher v-model="airdrop" />
+        <Switcher v-model="nftSettings.airdrop" />
       </div>
     </div>
   </Popup>
 </template>
 
 <script lang="ts" setup>
-import { ref, onBeforeUnmount, onMounted } from 'vue';
+import { reactive, onBeforeUnmount, onMounted } from 'vue';
 import { changeNftSettings } from '@/extension/messaging/nfts';
 import { accountController } from '@/controllers';
-import { type NftSettings } from '@/extension/background/extension-base/src/services/nft-service/types';
 
 const emit = defineEmits(['handleClose']);
-
-const spam = ref(false);
-const airdrop = ref(false);
+const nftSettings = reactive({ spam: false, airdrop: false });
 
 onMounted(() => {
   const settings = accountController.getNftSettings();
 
-  airdrop.value = !!settings.airdrop;
-  spam.value = !!settings.spam;
+  nftSettings.airdrop = !!settings.airdrop;
+  nftSettings.spam = !!settings.spam;
 });
 
 onBeforeUnmount(() => {
-  const settings: NftSettings = { spam: spam.value, airdrop: airdrop.value };
-
-  changeNftSettings(settings);
-  accountController.setNftSettings(settings);
+  changeNftSettings(nftSettings);
+  accountController.setNftSettings(nftSettings);
 });
 const onClose = () => emit('handleClose');
 </script>

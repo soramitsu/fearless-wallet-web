38973ed5519e7da8441929df60ff76a6fb3d1571
Евгений Галимов <egalimov95@gmail.com>
2024-01-24 00:41:00 +0100

    add placeholder

--- src/screens/wallet&asset/asset/NftDetails.vue
+++ src/screens/wallet&asset/asset/NftDetails.vue
@@ -3,12 +3,21 @@
     <Scroll>
       <div class="nft-details">
         <img v-if="image" :src="image" class="nft-details__img" :alt="id" width="500" height="500" />
+        <img
+          v-else
+          class="nft-details__img nft-details__img-placeholder"
+          src="@/assets/fearless-logo-animated.gif"
+          alt="nft__placeholder"
+          width="500"
+          height="300"
+        />
+
         <p class="nft-details__desc">{{ meta.description }}</p>
 
         <InfoRow text="nft.owned" :value="owned" />
         <InfoRow text="nft.id" :value="tokenId" />
-        <InfoRow text="common.network" :value="meta.address" />
-        <InfoRow text="nft.type" :value="type" />
+        <InfoRow text="common.network" :value="network" />
+        <InfoRow text="nft.type" :value="nft.type" />
 
         <div class="send-btn">
           <FButton
@@ -44,7 +53,7 @@
 <script lang="ts" setup>
 import { useRouter, useRoute } from 'vue-router/composables';
 import { computed } from 'vue';
-import type { FearlessNft, NftState } from '@extension-base/services/nft-service/types';
+import type { FearlessNft } from '@extension-base/services/nft-service/types';
 import { type SelectedWallet, useStore } from '@/store';
 import { cut } from '@/helpers';
 import { Components } from '@/router/routes';
@@ -54,32 +63,36 @@ const route = useRoute();
 const store = useStore();
 
 const id = computed(() => route.params.id);
-const nfts = computed<NftState>(() => store.getters.nfts ?? []);
 const contract = computed(() => route.params.contract);
-const ownedNfts = computed<FearlessNft[]>(() => nfts.value[contract.value]?.ownedNfts ?? []);
-ownedNfts;
-const nft = computed(() => ownedNfts.value.find((nft) => nft.id === id.value)!);
-const image = computed(() => nft.value?.image);
-const owned = computed(() => (nft.value?.isOwned ? 'owned' : 'not owned'));
-const isOwned = computed(() => nft.value?.isOwned);
-const meta = computed(() => nft.value?.meta ?? {});
-const type = computed(() => nft.value?.type);
-const tokenId = computed(() => cut(route.params.id, 5));
-const shareBtnType = computed(() => (isOwned.value ? 'secondary' : 'primary'));
-const selectedWallet = computed<SelectedWallet>(() => store.getters.selectedWallet);
+const nft = computed<Partial<FearlessNft>>(() => {
+  const nfts = store.getters.nfts ?? {};
 
+  const collection = nfts[contract.value] ?? {};
+  const ownedNfts: FearlessNft[] = collection.ownedNfts ?? [];
+
+  return ownedNfts.find((ownedNft) => ownedNft.id === id.value) ?? {};
+});
+
+const network = computed(() => nft.value.network ?? '');
+const image = computed(() => nft.value.image);
+const owned = computed(() => (nft.value.isOwned ? 'owned' : 'not owned'));
+const meta = computed(() => nft.value.meta ?? {});
+const tokenId = computed(() => cut(id.value, 5));
+const shareBtnType = computed(() => (nft.value.isOwned ? 'secondary' : 'primary'));
+const isOwned = computed(() => !!nft.value.isOwned);
 const onBack = () => router.back();
 const onSend = () => router.push({ name: Components.NftSendForm, params: { id: id.value } });
 
 const onShare = () => {
+  const selectedWallet: SelectedWallet = store.getters.selectedWallet;
   const dataToShare = {
-    'My public address to recieve NFTs:': selectedWallet.value.ethereumAddress,
+    'My public address to recieve NFTs:': selectedWallet.ethereumAddress,
     collection: contract.value,
-    owned: selectedWallet.value.ethereumAddress,
+    owned: selectedWallet.ethereumAddress,
     creator: nft.value.creator,
     network: nft.value.network,
-    'token Id': route.params.id,
-    type: type.value,
+    'token Id': id.value,
+    type: nft.value.type,
   };
 
   navigator.clipboard.writeText(JSON.stringify(dataToShare));
@@ -99,7 +112,9 @@ const onShare = () => {
     height: 500px;
     margin: 0 auto;
   }
-
+  &__img-placeholder {
+    height: 300px;
+  }
   &__desc {
     font-weight: 400;
     font-size: 14px;

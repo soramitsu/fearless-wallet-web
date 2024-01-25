10344e4b41802e69a1023cc15f70fbacc9fa0166
Maksim Baranov <33489381+MaksimBaranov98@users.noreply.github.com>
2024-01-26 00:09:17 +0300

    Fix bridge (#733)
    * fix
    
    * refactoring
    
    * fix

--- src/consts/sora.ts
+++ src/consts/sora.ts
@@ -17,9 +17,13 @@ const SORA_REWARD_ASSET = 'val';
 const SORA_ICON =
   'https://raw.githubusercontent.com/soramitsu/shared-features-utils/master/icons/chains/white/SORA.svg';
 
-const BRIDGE_MIN_VALUES: Record<string, number> = {
+const BRIDGE_MIN_VALUES_TO_SORA: Record<string, number> = {
   kusama: 0.05,
-  polkadot: 1,
+  polkadot: 1.1,
+};
+
+const BRIDGE_MIN_VALUES_FROM_SORA: Record<string, number> = {
+  polkadot: 1.1,
 };
 
 export {
@@ -31,5 +35,6 @@ export {
   SORA_REWARD_ASSET,
   SORA_ICON,
   SORA_VAL_ASSET_ID,
-  BRIDGE_MIN_VALUES,
+  BRIDGE_MIN_VALUES_TO_SORA,
+  BRIDGE_MIN_VALUES_FROM_SORA,
 };

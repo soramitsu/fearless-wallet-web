03b824b8612627b8e652d23c1350a80aead3d33d
Evgenii <egalimov95@gmail.com>
2024-02-07 11:22:26 +0100

    Bugfix/nft service (#772)
    * remove nft store
    
    * move excludeFilters method to nft service

--- src/extension/background/extension-base/src/stores/Nfts.ts
+++
@@ -1,9 +0,0 @@
-import SubscribableStore from '@extension-base/stores/SubscribableStore';
-import { EXTENSION_PREFIX } from '@extension-base/defaults';
-import { type NftStoreState } from '@extension-base/services/nft-service/types';
-
-export default class NftStore extends SubscribableStore<NftStoreState> {
-  constructor() {
-    super(`${EXTENSION_PREFIX}nfts`);
-  }
-}

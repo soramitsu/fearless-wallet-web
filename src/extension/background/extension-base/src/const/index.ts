77349d46aafedbd8f10d5e470c40910cc3cd4e8b
Евгений Галимов <egalimov95@gmail.com>
2024-02-08 17:42:59 +0100

    improve data flow

--- src/extension/background/extension-base/src/const/index.ts
+++ src/extension/background/extension-base/src/const/index.ts
@@ -12,3 +12,4 @@ export const ONBOARDING_URL = IS_PRODUCTION
 
 export const FALLBACK_LANG = 'en-EN';
 export const EXTENSION_ID = 'nhlnehondigmgckngjomcpcefcdplmgc';
+export const EXTENSION_HOSTNAME = '39fb1478-3519-4b4e-8eba-15e6e594494c';

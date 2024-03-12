61f79c0af7cf1ce9f1b0344e6a5a86492554f3a8
Евгений Галимов <egalimov95@gmail.com>
2024-03-12 11:33:31 +0100

    fix update balance after send

--- src/extension/background/extension-base/src/api/evm/transfer.ts
+++ src/extension/background/extension-base/src/api/evm/transfer.ts
@@ -165,9 +165,9 @@ export function getEVMTransactionObject(params: TransferParams): Promise<Transac
 export function makeEVMTransfer(params: MakeTransferParams): Promise<void> {
   const transfer = params.balance.isUtility ? makeUtilityTransfer(params) : makeERC20Transfer(params);
 
-  return transfer.then(() => {
+  return transfer.finally(() => {
     setTimeout(() => {
       state.fetchEvmBalance({ ethereumAddress: params.from, assetId: params.balance.id, force: true });
-    }, 10000);
+    }, 5000);
   });
 }

cb6461abd2481faacae8ed39aef2ace217aed492
MaksimBaranov98 <maks.baranow2011@yandex.ru>
2024-03-18 18:18:46 +0300

    fix dapp connect

--- vue.config.base.js
+++ vue.config.base.js
@@ -28,6 +28,7 @@ module.exports = {
       const def = definitions[0]['process.env'];
 
       def.EXTENSION_PREFIX = JSON.stringify(process.env.EXTENSION_PREFIX);
+      def.IS_EXTENSION = JSON.stringify(process.env.IS_EXTENSION);
       def.OAUTH_CLIENT_ID = JSON.stringify(process.env.OAUTH_CLIENT_ID);
       def.PORT_PREFIX = JSON.stringify(process.env.PORT_PREFIX);
       def.RAMP_TEST_API_KEY = JSON.stringify(process.env.RAMP_TEST_API_KEY);

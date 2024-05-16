77a511593867826941a4e79ce0c8c338e666d36a
MaksimBaranov98 <maks.baranow2011@yandex.ru>
2024-05-16 17:57:24 +0300

    types

--- src/store/types.ts
+++ src/store/types.ts
@@ -4,8 +4,9 @@ import type { State as AccountState } from '@/store/accounts/state';
 import type { State as ExtensionState } from '@/store/extension/state';
 import type { State as SoraCardState } from '@/store/soraCard/state';
 import type { State as StakingState } from '@/store/staking/state';
+import type { State as PoolsState } from '@/store/pools/state';
 
-type ModulesTypes = NetworksState & AccountState & ExtensionState & SoraCardState & StakingState;
+type ModulesTypes = NetworksState & AccountState & ExtensionState & SoraCardState & StakingState & PoolsState;
 type Modules = ModuleTree<ModulesTypes>;
 
 export default Modules;

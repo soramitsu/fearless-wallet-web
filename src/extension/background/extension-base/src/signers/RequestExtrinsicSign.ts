a57b01360b6dded81ca82ebbe037afe7f22543e4
Maksim Baranov <33489381+MaksimBaranov98@users.noreply.github.com>
2024-06-24 14:12:35 +0300

    fix (#911)

--- src/extension/background/extension-base/src/signers/RequestExtrinsicSign.ts
+++ src/extension/background/extension-base/src/signers/RequestExtrinsicSign.ts
@@ -1,19 +1,21 @@
 import { type TypeRegistry } from '@polkadot/types';
-import type State from '@extension-base/background/handlers/State';
+import { state } from '@extension-base/background/handlers';
 import type { KeyringPair } from '@polkadot/keyring/types';
 import type { SignerPayloadJSON } from '@polkadot/types/types';
 import type { HexString } from '@polkadot/util/types';
 import type { RequestSign } from '@extension-base/background/types/types';
 
 export default class RequestExtrinsicSign implements RequestSign {
-  constructor(
-    public readonly payload: SignerPayloadJSON,
-    public readonly state: State,
-    private readonly isMobile = false
-  ) {}
+  public readonly payload: SignerPayloadJSON;
+  private readonly isMobile: boolean;
+
+  constructor(payload: SignerPayloadJSON, isMobile = false) {
+    this.payload = payload;
+    this.isMobile = isMobile;
+  }
 
   async sign(registry: TypeRegistry, pair: KeyringPair): Promise<{ signature: HexString }> {
-    if (this.isMobile) return this.state.walletConnectDappService.onRequest(this.payload);
+    if (this.isMobile) return state.walletConnectDappService.onRequest(this.payload);
 
     const signData = registry.createType('ExtrinsicPayload', this.payload, { version: this.payload.version });
 

906a444cf0328e8fc43f786dbb5971efa90898cf
MaksimBaranov98 <maks.baranow2011@yandex.ru>
2024-07-09 18:46:49 +0300

    fixes

--- src/extension/background/extension-base/src/signers/RequestBytesSign.ts
+++ src/extension/background/extension-base/src/signers/RequestBytesSign.ts
@@ -1,21 +1,17 @@
 import { wrapBytes } from '@polkadot/extension-dapp/wrapBytes';
 import { type TypeRegistry } from '@polkadot/types';
 import { u8aToHex } from '@polkadot/util';
-import type State from '@extension-base/background/handlers/State';
+import { state } from '@extension-base/background/handlers';
 import type { KeyringPair } from '@polkadot/keyring/types';
 import type { SignerPayloadRaw } from '@polkadot/types/types';
 import type { HexString } from '@polkadot/util/types';
 import type { RequestSign } from '@extension-base/background/types/types';
 
 export default class RequestBytesSign implements RequestSign {
-  constructor(
-    public readonly payload: SignerPayloadRaw,
-    public readonly state: State,
-    private readonly isMobile = false
-  ) {}
+  constructor(public readonly payload: SignerPayloadRaw, private readonly isMobile = false) {}
 
   async sign(_registry: TypeRegistry, pair: KeyringPair): Promise<{ signature: HexString }> {
-    if (this.isMobile) return this.state.walletConnectDappService.onRequestRaw(this.payload);
+    if (this.isMobile) return state.walletConnectDappService.onRequestRaw(this.payload);
 
     return {
       signature: u8aToHex(pair.sign(wrapBytes(this.payload.data))),

007840377f9ca5bf2a8c8818f20bc2962497ff13
bertikaz <dominantpk@gmail.com>
2024-09-25 12:26:47 +0200

    getId function was changed

--- src/extension/background/extension-base/src/utils/utils.ts
+++ src/extension/background/extension-base/src/utils/utils.ts
@@ -4,6 +4,7 @@ import { isEthereumAddress, decodeAddress, encodeAddress, ethereumEncode } from 
 import { EXTENSION_REQUEST_URL } from '@extension-base/const';
 import type { NetworkJson } from '@extension-base/types';
 import type { KeypairType } from '@polkadot/util-crypto/types';
+import { IS_EXTENSION } from '@/consts/global';
 
 export function sumBN(inputArr: BN[]) {
   let rs = new BN(0);
@@ -18,7 +19,7 @@ export function sumBN(inputArr: BN[]) {
 let counter = 0;
 
 export function getId(message = ''): string {
-  return `${EXTENSION_PREFIX}.${++counter}.${message}`;
+  return `${EXTENSION_PREFIX}.${IS_EXTENSION ? Date.now() : ''}.${++counter}.${message}`;
 }
 
 export function canDerive(type?: KeypairType): boolean {
